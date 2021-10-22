952932fa553cdb739ab1e1ac714476f4d6e9ee43
Yaakov Haimoff <hyaakov@aol.com>
2021-10-22 07:32:36 +0300

    rectangle almost finished

--- include/Rectangle.h
+++ include/Rectangle.h
@@ -2,24 +2,29 @@
 
 #include <iosfwd>
 #include "Vertex.h"
+#include "Board.h"
 
 class Rectangle
 {
 public:
     Rectangle(const Vertex &bottomLeft, const Vertex &topRight); // 1
     Rectangle(const Vertex vertices[2]);
-    Rectangle(double x0, double y0, double x1, double y1);
     Rectangle(const Vertex& start, double width, double height);
+    Rectangle(double x0, double y0, double x1, double y1);
     ~Rectangle();
 
-    Vertex getBottomLeft() const; // 1
-    Vertex getTopRight() const;   // 1
+    Vertex getBottomLeft() const; 
+    Vertex getTopRight() const;   
     double getWidth() const;
     double getHeight() const;
+    
+    void draw(Board& board) const;
+    Rectangle getBoundingRectangle()const;
+    double getArea()const;
+    Vertex getCenter()const;
+    bool scale(double factor);
 
 private:
-    double m_bottom_left_x;
-    double m_bottom_left_y;
-    double m_top_right_x;
-    double m_top_right_y;
+    Vertex m_bottomLeft;
+    Vertex m_rightTop;
 };
--- src/Rectangle.cpp
+++ src/Rectangle.cpp
@@ -1,17 +1,54 @@
 #include "Rectangle.h"
 #include "Vertex.h"
+#include <iostream>
+#include <cmath>
 
 #pragma once
 #include <iosfwd>
 
-Vertex getBottomLeft() const // 1
+Rectangle::Rectangle(const Vertex &bottomLeft, const Vertex &topRight)
 {
+    if (m_bottomLeft.isHigherThan(m_rightTop) ||
+        m_bottomLeft.isToTheRightOf(m_rightTop) ||
+        !m_bottomLeft.isValid() || !m_rightTop.isValid())
+    {
+        m_bottomLeft = Vertex(20, 10);
+        m_rightTop = Vertex(30, 20);
+    }
 }
-double getWidth() const
-{
-    
-}
-double getHeight() const
-{
 
+Rectangle::Rectangle(const Vertex vertices[2])
+{
+    Rectangle(vertices[0], vertices[1]);
+}
+
+Rectangle::Rectangle(const Vertex &start, double width, double height)
+{
+    m_rightTop.m_col = width + m_bottomLeft.m_row;
+    m_rightTop.m_row = height + m_bottomLeft.m_col;
+    Rectangle(start, m_rightTop);
+}
+
+Rectangle::Rectangle(double x0, double y0, double x1, double y1)
+{
+    Rectangle(Vertex(x0, y0), Vertex(x1, y1));
+}
+
+Vertex Rectangle::getBottomLeft() const
+{
+    return m_bottomLeft;
+}
+Vertex Rectangle::getTopRight() const
+{
+    return m_rightTop;
+}
+
+double Rectangle::getHeight() const
+{
+    return abs(m_rightTop.m_row - m_bottomLeft.m_row);
+}
+
+double Rectangle::getWidth() const
+{
+    return abs(m_rightTop.m_col + m_bottomLeft.m_col);
 }
