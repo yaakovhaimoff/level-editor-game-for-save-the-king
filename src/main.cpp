ccd9890c6956acc3450357a5ab11a9e37749f26a
Yaakov Haimoff <hyaakov@aol.com>
2021-10-27 20:40:33 +0300

    ready for checking

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@
 #include "Vertex.h"
 #include "Board.h"
 
-#define RECTANGLE           1
-#define TRIANGLE            1
-#define SQUARE              1
+#define RECTANGLE           0
+#define TRIANGLE            0
+#define SQUARE              0
 #define HOURGLASS           1
 
 #if RECTANGLE
